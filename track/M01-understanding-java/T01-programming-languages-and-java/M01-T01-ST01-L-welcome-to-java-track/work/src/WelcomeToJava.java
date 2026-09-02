648bafc32225f496af33e6769df253679d97fb41
Chitrarasu-J <chitrarasu33@gmail.com>
2026-09-02 10:55:01 +0530

    This is my first update

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,9 @@
+
 public class WelcomeToJava {
+
     public static void main(String[] args) {
-       //Write your code here
+        System.out.println("Welcome to Java");
+        System.out.println("I am in Java Track");
+        //Write your code here
     }
 }
