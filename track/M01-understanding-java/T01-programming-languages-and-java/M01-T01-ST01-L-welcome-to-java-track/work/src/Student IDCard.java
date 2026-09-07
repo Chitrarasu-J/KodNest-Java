6e944661dcfe349333d4c8583a5e33557a1cef93
Chitrarasu-J <chitrarasu33@gmail.com>
2026-09-07 11:11:37 +0530

    new files

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Student IDCard.java
@@ -0,0 +1,14 @@
+
+class Main {
+
+    public static void main(String[] args) {
+        System.out.println("--------------------");
+        System.out.println("STUDENT PROFILE");
+        System.out.println("NAME: Alex");
+        System.out.println("Course: Java");
+        System.out.println("Level: Beginner");
+        System.out.println("--------------------");
+
+    }
+
+}
