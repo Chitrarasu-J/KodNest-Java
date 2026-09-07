220bad0c5ebfbe4a66d144e2c3e0d9b714e05517
Chitrarasu-J <chitrarasu33@gmail.com>
2026-09-07 11:05:02 +0530

    New Files

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -2,8 +2,8 @@
 public class WelcomeToJava {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java \njava is good");
         System.out.println("I am in Java Track");
-        //Write your code here
+
     }
 }
