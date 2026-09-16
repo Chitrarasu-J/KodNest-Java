17ed724790c7bc5c705ee33c82547e1c4c884adb
Chitrarasu-J <chitrarasu33@gmail.com>
2026-09-16 18:16:54 +0530

    New files

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/SimpleInterest.java
@@ -0,0 +1,31 @@
+
+public class SimpleInterest {
+
+    public static void main(String[] args) {
+
+        double principle = 10000.0;
+
+        double rate = 6.5;
+
+        double time = 2.0;
+
+        double simpleInterest = principle * rate * time / 100.0;
+
+        System.out.println("Simple Interest: " + simpleInterest);
+
+        System.out.println("Total Amount: " + (principle + simpleInterest));
+
+        double Weight = 72.0;
+
+        double height = 1.8;
+
+        System.out.println("BMI: " + (Weight / (height * height)));
+
+        int totalMarks = 78 + 84 + 69 + 91 + 88;
+
+        System.out.println("Total Marks: " + totalMarks);
+
+        System.out.println("Percentage: " + (totalMarks * 100.0) / 500);
+
+    }
+}
